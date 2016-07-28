80fd6f861edb4214c7d333521a95885f04a4b53c
Sobolev Kirill <kirrok@mail.ru>
2016-07-28 18:05:36 +0000

    Начало работы с базой

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class set(models.Model):
+	TYPES= (
+		('S','Standart'),
+		('P','Premium'),
+	)
+	kind = models.CharField(max_length=2, choices=TYPES)
+	calories = models.IntegerField()
+	protein = models.IntegerField()
+	carbohydrates = models.IntegerField()
+	fat = models.IntegerField()
+	price = models.IntegerField()
+	image = models.ImageField(blank=True)
+
+
