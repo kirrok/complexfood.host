59df5d76cdc4f5f726f2c2a879e4e8a1e26c4a71
Sobolev Kirill <k.sobolev@securitycode.ru>
2016-07-28 19:19:33 +0300

    comm

--- main_app/views.py
+++ main_app/views.py
@@ -8,4 +8,18 @@ def index(request):
 def base(request):
 	return render(request, 'base.html')
 
+def menu(request):
+	return render(request, 'menu.html')
+
+def learn_more(request):
+	return render(request, 'learn_more.html')
+
+def questions(request):
+	return render(request, 'questions.html')
+
+def feedback(request):
+	return render(request, 'feedback.html')
+
+def contacts(request):
+	return render(request, 'contacts.html')
 # Create your views here.
