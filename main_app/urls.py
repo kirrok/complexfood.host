2083411ab86486d4284dcf3baf60ad5c1f8ed179
Sobolev Kirill <k.sobolev@securitycode.ru>
2016-07-28 17:37:53 +0300

    base.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.index, name='index'),
+	url(r'^base$',views.base, name='base'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,5 +5,7 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'index.html')
 
+def base(request):
+	return render(request, 'base.html')
 
 # Create your views here.
